93abed54afc30c7b578c7f15921c1f69e1887340
t17009467 <loumi.tremas@etu.univ-amu.fr>
2018-11-30 09:20:43 +0100

    constructeurs corrigés

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,27 +1,41 @@
 package image;
 
 import java.awt.*;
+import java.nio.file.NoSuchFileException;
+
 import javafx.scene.paint.Color;
+import util.Matrices;
+
 public class BruteRasterImage implements Image {
     int width ;
     int height;
-    public Color[][] pixels;
-    Color color;
+    public Color[][] colors;
+
 
     public BruteRasterImage(int width, int height, Color color) {
-     Color [][] pixels = new Color[width][height];
+
      this.width=width;
      this.height=height;
 
-     for(int i=0;i<width; i++){
+     for (int i=0;i<width; i++){
          for (int j=0;j<height;j++){
-             pixels[i][j] = getPixelColor(i,j);
+             colors[i][j] = color;
          }
      }
+
     }
-    public BruteRasterImage(Color[][] color){
-        width=pixels.length;
-        height=pixels[0].length;
+    public BruteRasterImage(Color[][] colors){
+
+
+
+
+        width=colors.length;
+        height=colors[0].length;
+        Matrices.requiresNonZeroDimensions(colors);
+        Matrices.requiresRectangularMatrix(colors);
+
+
+        this.colors=colors.clone();
     }
 
 
@@ -36,6 +50,18 @@ public class BruteRasterImage implements Image {
     }
     @Override
     public Color getPixelColor(int x,int y) {
-        return pixels[x][y];
+        return colors[x][y];
+    }
+
+    protected void setWidth(int width) {
+        this.width = width;
+    }
+
+    protected void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void createRepresentation(){
+
     }
 }
