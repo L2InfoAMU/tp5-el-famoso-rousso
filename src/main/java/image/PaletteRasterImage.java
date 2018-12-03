550eaec3af9debf54370b6418bbb17ff90bc1137
l17020242 <johan.lorenzatti@etu.univ-amu.fr>
2018-12-03 16:13:18 +0100

    Pallette raster image crée et tous les changements avec

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -1,4 +1,74 @@
 package image;
 
-public class PaletteRasterImage {
+
+import javafx.scene.paint.Color;
+import util.Matrices;
+
+public class PaletteRasterImage implements Image {
+
+    int width ;
+    int height;
+    public Color[][] colors;
+
+
+    public PaletteRasterImage(int width, int height, Color color) {
+        this.width = width;
+        this.height = height;
+        colors = new Color[width][height];
+
+         for (int i=0;i<width; i++){
+            for (int j=0;j<height;j++){
+                colors[i][j] = color;
+            }
+        }
+    }
+
+    public PaletteRasterImage(Color[][] colors) {
+        width=colors.length;
+        height=colors[0].length;
+        Matrices.requiresNonZeroDimensions(colors);
+        Matrices.requiresRectangularMatrix(colors);
+
+
+        this.colors=colors.clone();
+    }
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+    @Override
+    public Color getPixelColor(int x,int y) {
+        return colors[x][y];
+    }
+
+    protected void setWidth(int width) {
+        this.width = width;
+    }
+
+    protected void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void createRepresentation(){
+
+        colors = new Color[getWidth()][getHeight()];
+    }
+    public void setPixelColor(Color color, int x, int y){
+        this.colors[x][y]=color;
+    }
+    private void setPixelsColor(Color[][] pixels){
+        this.colors = pixels.clone();
+    }
+    private void setPixelsColor(Color color) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.colors[i][j] = color;
+            }
+        }
+    }
 }
