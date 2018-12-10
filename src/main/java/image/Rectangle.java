34ac6f4b4086fe88420bce8289f5bfe651f1e1b3
t17009467 <loumi.tremas@etu.univ-amu.fr>
2018-12-10 16:03:22 +0100

    rectangle ok

---
+++ src/main/java/image/Rectangle.java
@@ -0,0 +1,25 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+public class Rectangle implements Shape {
+
+    int x, y, width, height;
+    Color color;
+
+    public Rectangle(int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    @Override
+    public boolean contains(Point point) {
+        return this.x < point.x && point.x <= this.x+width && this.y < point.y && point.y<= this.y+height;
+    }
+    public Color getColor(){
+        return color;
+    }
+}
